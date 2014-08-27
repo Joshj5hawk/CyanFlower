7e7b65ddfc169112a9e0035f9ce757bb201c3777
joshj5hawk <joshj5hawk@gmail.com>
2014-08-26 21:33:50 -0500

    Got flowers to generate. WOOT.
    Signed-off-by: joshj5hawk <joshj5hawk@gmail.com>

--- src/main/java/com/joshj5hawk/handler/CFWorldGen.java
+++ src/main/java/com/joshj5hawk/handler/CFWorldGen.java
@@ -1,16 +1,21 @@
 package com.joshj5hawk.handler;
 
 import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenFlowers;
 import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraftforge.event.terraingen.TerrainGen;
+
+import com.joshj5hawk.main.CyanFlower;
+
 import cpw.mods.fml.common.IWorldGenerator;
 
 
 public class CFWorldGen implements IWorldGenerator
 {
-
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider cG, IChunkProvider cP) 
 	{
@@ -27,10 +32,11 @@ public class CFWorldGen implements IWorldGenerator
 				generateEnd(world, random, chunkX*16, chunkZ*16);
 		}
 	}
-
+	
 	private void generateSurface(World world, Random random, int x, int z) 
 	{
-		//this.addOreSpawn(BLOCK, world, random, x, z, maxX, MaxZ, maxVeinSize, percentChanceToSpawn, minY, maxY);
+		//this.addOreSpawn(BLOCK, world, random, x, z, maxX(16), maxZ(16), maxVeinSize, percentChanceToSpawn, minY, maxY);
+		this.generateFlower(CyanFlower.blockCyanFlower, world, random, x, z);
 	}
 
 	private void generateNether(World world, Random random, int x, int z) 
@@ -53,4 +59,16 @@ public class CFWorldGen implements IWorldGenerator
 			(new WorldGenMinable(block, maxVeinSize)).generate(world, random, posX, posY, posZ);
 		}
 	}
+	private void generateFlower(Block block, World world, Random random, int x, int z)
+	{
+		
+		for(int i = 0; i < 2; i ++)
+		{
+			int posX = x + random.nextInt(16) + 8;
+			int posY = random.nextInt(256);
+			int posZ = z + random.nextInt(16) + 8;
+			new WorldGenFlowers(block).generate(world, random, posX, posY, posZ);
+		}
+	}
+	
 }
